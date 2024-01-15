e989f2f9b9abdb774fd2e11bc1a4e7560bebf0c8
iniyan88 <biniyan2004@gmail.com>
2024-01-15 19:34:14 +0530

    dateStringChange

--- src/_main/components/PostCard.tsx
+++ src/_main/components/PostCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import { timeAgo } from "@/lib/utils";
 type PostCardProps = {
   post: Models.Document;
 };
@@ -9,20 +10,27 @@ const PostCard = ({ post }: PostCardProps) => {
     <div className="post-card">
       <div className="flex-between">
         <div className="flex items-center gap-3  text-slate-100">
-          {/* <Link to={`/profile/${post.creator.$id}`}></Link> */}
-          <img
-            src={post?.creator?.imgUrl || "/public/assets/react.svg"}
-            alt="creator"
-            className="rounded-full w-12 lg:h-12"
-          />
+          <Link to={`/profile/${post.creator.$id}`}>
+            <img
+              src={post?.creator?.imgUrl || "/public/assets/react.svg"}
+              alt="creator"
+              className="rounded-full w-12 lg:h-12"
+            />
+          </Link>
           <div className="flex flex-col">
             <p> {post.creator.name}</p>
-            <div>
-              <p>{post.$createdAt}</p>
+            <div className="flex-center gap-2 text-light-3">
+              <p className="subtle-semibold lg:small-regular">
+                {timeAgo(post.$createdAt)}
+              </p>
+              -
+              <p className="sublt-semibold lg:small-regular">{post.location}</p>
             </div>
-            -<div>{post.location}</div>
           </div>
         </div>
+        <Link to={`/update-post/${post.$id}`}>
+          <img src="" alt="" />
+        </Link>
       </div>
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,33 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+export function timeAgo(time: string) {
+  const date = new Date(time);
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 31);
+  const years = Math.floor(months / 12);
+  if (years > 0) {
+    return `${years} years ago`;
+  } else if (months > 0) {
+    return `${months} months ago`;
+  } else if (days > 0) {
+    return `${days} days ago`;
+  } else if (hours > 0) {
+    return `${hours} hours ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minutes ago`;
+  } else {
+    return `${seconds} seconds ago`;
+  }
+}
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
